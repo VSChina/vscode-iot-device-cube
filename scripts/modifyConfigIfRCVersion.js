26c3f4d67cd189f3845aefd04c6591c8c69c3189
dilin-MS <dilin@microsoft.com>
2020-08-25 14:43:14 +0800

    Use same name with prod for RC bit and do not publish to marketplace

--- scripts/modifyConfigIfRCVersion.js
+++ scripts/modifyConfigIfRCVersion.js
@@ -5,20 +5,12 @@ if (process.env.TRAVIS_TAG) {
   if (isTestVersion) {
     const packageJson = JSON.parse(fs.readFileSync('package.json'));
 
-    const testName = "test-albatross";
-    const testDisplayName = "Test Albatross RC";
-    const testPublisher = "IoTDevExBuild";
-    packageJson.name = testName;
-    packageJson.displayName = testDisplayName;
-    packageJson.publisher = testPublisher;
     packageJson.aiKey = process.env['TEST_AIKEY'];
 
     const indexOfDash = packageJson.version.indexOf('-');
     if (indexOfDash > 0) {
       packageJson.version = packageJson.version.substring(0, indexOfDash);
     }
-
-    delete packageJson.icon;
 
     fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n');
   }
